48e1cd05b2773059746ee3e9080818c5ac08109f
永富敬千 <taka@nagatomitakashisennoMacBook-Pro.local>
2024-10-28 21:56:07 +0900

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: vercel(),
+  site: 'https://team-raga.github.io',
+  base: '/Kanban',
   integrations: [
     tailwind({}),
     react()
